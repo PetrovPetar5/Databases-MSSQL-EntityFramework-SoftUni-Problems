068b6ab38c436be25f0110ccf52af7838ae3a69e
PetrovPetar5 <74185222+PetrovPetar5@users.noreply.github.com>
2021-02-18 22:29:03 +0200

    Add files via upload

---
+++ Entity Framework Core/ADO.NET/ChangeTownNamesCasing/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace ChangeTownNamesCasing
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.Data.SqlClient;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = @"Server=.;Database=MinionsDB;Integrated Security=true;";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string country = Console.ReadLine();
+                string citiesQuery = @"UPDATE Towns
+   SET Name = UPPER(Name)
+ WHERE CountryCode = (SELECT c.Id FROM Countries AS c WHERE c.Name = @countryName)";
+                using (SqlCommand updateCities = new SqlCommand(citiesQuery, connection))
+                {
+                    updateCities.Parameters.AddWithValue("@countryName", country);
+                    var updatedCities = updateCities.ExecuteNonQuery();
+                    if (updatedCities == 0)
+                    {
+                        Console.WriteLine("No town names were affected.");
+                    }
+                    else
+                    {
+                        string citiesAffected = @"SELECT t.Name 
+   FROM Towns as t
+   JOIN Countries AS c ON c.Id = t.CountryCode
+  WHERE c.Name = @countryName";
+                        using (SqlCommand affectedCities = new SqlCommand(citiesAffected, connection))
+                        {
+                            affectedCities.Parameters.AddWithValue("@countryName", country);
+                            using (var result = affectedCities.ExecuteReader())
+                            {
+                                List<string> countries = new List<string>();
+                                while (result.Read())
+                                {
+                                    countries.Add(result[0].ToString());
+                                }
+
+                                Console.WriteLine($"{updatedCities} town names were affected.");
+                                Console.WriteLine($"[{ String.Join(", ", countries)}]");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Entity Framework Core/ADO.NET/MinionNames/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace MinionNames
+{
+    using System;
+    using Microsoft.Data.SqlClient;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = @"Server=.;Database=MinionsDB;Integrated Security=true;";
+            int id = int.Parse(Console.ReadLine());
+            string commandString = @"SELECT Name FROM Villains WHERE Id = @Id";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand getsVillainName = new SqlCommand(commandString, connection))
+                {
+                    getsVillainName.Parameters.AddWithValue("@Id", id);
+                    var nameResult = getsVillainName.ExecuteScalar();
+                    if (nameResult == null)
+                    {
+                        Console.WriteLine($"No villain with ID {id} exists in the database.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Villain: {nameResult}");
+                        string villainMinions = @"SELECT ROW_NUMBER() OVER (ORDER BY m.Name) as RowNum,
+                                         m.Name, 
+                                         m.Age
+                                    FROM MinionsVillains AS mv
+                                    JOIN Minions As m ON mv.MinionId = m.Id
+                                   WHERE mv.VillainId = @Id
+                                ORDER BY m.Name";
+                        using (SqlCommand getsVillainMinions = new SqlCommand(villainMinions, connection))
+                        {
+                            getsVillainMinions.Parameters.AddWithValue("@Id", id);
+                            using (var minionsInfo = getsVillainMinions.ExecuteReader())
+                            {
+                                if (!minionsInfo.HasRows)
+                                {
+                                    Console.WriteLine("(no minions)");
+                                }
+                                else
+                                {
+                                    while (minionsInfo.Read())
+                                    {
+                                        Console.WriteLine($"{minionsInfo[0]}. {minionsInfo[1]} {minionsInfo[2]}");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
